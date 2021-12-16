9acfa2beb50ddb3acac324d00ea316ae1afa79a6
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2021-12-16 11:40:07 +0100

    Arreglado el error, los dos bucles de los espacios y los # tienen que estar dentro del bucle principal

--- Ejercicio 4 La escalera.py
+++ Ejercicio 4 La escalera.py
@@ -1,11 +1,11 @@
 def escalera(tamaño):
-    for i in range(0,tamaño): #Bucle para crear las líneas
+    for i in range(0,tamaño):  #Bucle para crear las líneas
         linea=""
-    for j in range(0,tamaño-1-i): #Bucle para dejar espacios
-        linea=linea + " "
-    for k in range(0,i+1):
-        linea = linea + "# " #Bucle para printear la escalera
-    print(linea)
+        for j in range(0,tamaño-1-i):  #Bucle para dejar los espacios en las líneas
+            linea=linea + " "
+        for k in range(0,i+1):
+            linea = linea + "# "  #Bucle para printear los # de la escalera
+        print(linea)
 
 
 
