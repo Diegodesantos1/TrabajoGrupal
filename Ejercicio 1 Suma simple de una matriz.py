fa6ec02a4c79fca5ed0f110620855815f59b1d8a
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2021-12-16 11:10:37 +0100

    Puesto en formato pep8

--- Ejercicio 1 Suma simple de una matriz.py
+++ Ejercicio 1 Suma simple de una matriz.py
@@ -3,14 +3,19 @@ import os
 import random
 import re
 import sys
+
+
 def Suma(numeros):
-    suma=0
+    suma = 0
     for i in numeros:
-        suma= suma+i
+        suma = suma + i
     return suma
 
-if __name__ == '__main__':
-    fptr = sys.stdout #Arreglado el error que no dejaba funcionar el código, de stackoverflow
+
+if __name__ == "__main__":
+    fptr = (
+        sys.stdout
+    )  # Arreglado el error que no dejaba funcionar el código, de stackoverflow
     print("¿De qué tamaño quieres la matriz?")
     ar_count = int(input().strip())
     print("Introduzca los números a sumar separados por un espacio")
@@ -18,6 +23,6 @@ if __name__ == '__main__':
 
     resultado = Suma(numeros)
 
-    fptr.write(str(resultado) + '\n')
+    fptr.write(str(resultado) + "\n")
 
     fptr.close()
