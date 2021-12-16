620ce98eeaaa429f09c5c09ef5b70e9d0b3d5246
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2021-12-16 11:13:07 +0100

    Copiado el enunciado

---
+++ Ejercicio 4 La escalera.py
@@ -0,0 +1,14 @@
+import math
+import os
+import random
+import re
+import sys
+def staircase(n):
+# Write your code here
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    staircase(n)
