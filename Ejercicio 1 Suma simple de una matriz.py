494e5b8d5950190e1b4e4d0e234105e67b043d5e
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2021-12-16 11:00:33 +0100

    Puestas las variables en español y creados los print para que quede claro qué hay que introducir

--- Ejercicio 1 Suma simple de una matriz.py
+++ Ejercicio 1 Suma simple de una matriz.py
@@ -3,10 +3,11 @@ import os
 import random
 import re
 import sys
-def SumaSimple(ar):
+def SumaSimple(numeros):
     suma = 0 #Comienza en 0 la suma
     for i in ar:
         suma = suma + i
+    return suma
 
 
 
@@ -14,9 +15,13 @@ def SumaSimple(ar):
 
 
 if __name__ == '__main__':
-fptr = open(os.environ['OUTPUT_PATH'], 'w')
-ar_count = int(input().strip())
-ar = list(map(int, input().rstrip().split()))
-result = simpleArraySum(ar)
-fptr.write(str(result) + '\n')
-fptr.close()
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    print("¿De qué tamaño quieres la matriz?")
+    tamaño_matriz = int(input().strip())
+    print("Introduzca los números a sumar separados por un espacio")
+    numeros= list(map(int, input().rstrip().split()))
+    
+    result = SumaSimple(ar)
+    fptr.write(str(result) + '\n')
+    
+    fptr.close()
