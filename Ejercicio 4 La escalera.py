e9bf117d131432b66958664a01e546873d4cd02c
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2021-12-16 11:29:55 +0100

    Creada otra parte del bucle, pero sigue sin funcionar

--- Ejercicio 4 La escalera.py
+++ Ejercicio 4 La escalera.py
@@ -6,10 +6,13 @@ import sys
 def escalera(tamaño):
     for i in range(0,tamaño): #Bucle para crear las líneas
         linea=" "
+    for j in range(0,tamaño-1-i):
+            linea=linea + " "
 
 
 
 if __name__ == '__main__':
+    print("Introduzca el tamaño de la escalera")
     tamaño = int(input().strip())
 
     escalera(tamaño)
