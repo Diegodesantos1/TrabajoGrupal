fe3c11ee66d470844f279f75ffd954ea7a544349
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2021-12-16 11:40:33 +0100

    Puesto en formato pep8

--- Ejercicio 4 La escalera.py
+++ Ejercicio 4 La escalera.py
@@ -1,15 +1,16 @@
 def escalera(tamaño):
-    for i in range(0,tamaño):  #Bucle para crear las líneas
-        linea=""
-        for j in range(0,tamaño-1-i):  #Bucle para dejar los espacios en las líneas
-            linea=linea + " "
-        for k in range(0,i+1):
-            linea = linea + "# "  #Bucle para printear los # de la escalera
+    for i in range(0, tamaño):  # Bucle para crear las líneas
+        linea = ""
+        for j in range(
+            0, tamaño - 1 - i
+        ):  # Bucle para dejar los espacios en las líneas
+            linea = linea + " "
+        for k in range(0, i + 1):
+            linea = linea + "# "  # Bucle para printear los # de la escalera
         print(linea)
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     print("Introduzca el tamaño de la escalera")
     tamaño = int(input().strip())
 
