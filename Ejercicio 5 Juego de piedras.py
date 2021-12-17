08c497b839534b33c9da19f41ea87cc02338f004
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2021-12-17 19:32:09 +0100

    creada la funcion ejecutar juego y cambiado el nombre de la función

--- Ejercicio 5 Juego de piedras.py
+++ Ejercicio 5 Juego de piedras.py
@@ -6,13 +6,13 @@ import sys
 
 def JuegoPiedras(n):
     ganador=""
-    if(jugada(n)!=0):
+    if(jugadabuena(n)!=0):
         ganador="El jugador 1 es el ganador del juego de las piedras"
     else:
         ganador ="El jugador 2 es el ganador del juego de las piedras"
     return ganador
 
-def jugada(n):
+def jugadabuena(n):
     buenajugada=0
     modulo=n%7
     if(modulo>=2 and modulo<=3):
@@ -24,6 +24,26 @@ def jugada(n):
     return
 
 
+def ejecutarjuego(n):
+    intentos=0
+    ganador=""
+    while(ganador!="El jugador 1 es el ganador del juego de las piedras" and ganador!="El jugador 2 es el ganador del juego de las piedras"):
+        jugada=jugadabuena(n)
+        if(jugada==0):
+            if(n>5):
+                jugada=5
+            elif(n>3):
+                jugada=3
+            else:
+                jugada=2
+        print("Juega P"+str((intentos%2)+1) +" quitando "+ str(jugada)+" piedras")
+        n=n-jugada
+        if(n==1 or n==0):
+            ganador=("P"+str((intentos%2)+1)+" is the winner")
+        intentos+=1
+    print(ganador)
+
+
 if __name__ == '__main__':
     fptr = (sys.stdout)  # Arreglado el error que no dejaba funcionar el código, de stackoverflow
     print("Cuántas partidas quieres jugar?")
@@ -32,5 +52,6 @@ if __name__ == '__main__':
         print("¿Cuántas piedras tienes?")
         n = int(input().strip())
         result = JuegoPiedras(n)
+        ejecutarjuego(n)
         fptr.write(result + '\n')
     fptr.close()
