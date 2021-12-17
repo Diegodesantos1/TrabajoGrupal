a5c0638c5d97725d61dc9085529c4b66c7e3299d
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2021-12-17 21:52:46 +0100

    Puesto los mensajes en inglés para que se arregle el error del bucle infinito, ya que el main te pide que sea en inglés para que runee perfectamente

--- Ejercicio 5 Juego de piedras.py
+++ Ejercicio 5 Juego de piedras.py
@@ -6,13 +6,13 @@ import sys
 
 def JuegoPiedras(n):
     ganador=""
-    if(jugadabuena(n)!=0):
-        ganador="El jugador 1 es el ganador del juego de las piedras"
+    if(jugadaperfecta(n)!=0):
+        ganador="P1 is the winner"
     else:
-        ganador ="El jugador 2 es el ganador del juego de las piedras"
+        ganador ="P2 is the winner"
     return ganador
 
-def jugadabuena(n):
+def jugadaperfecta(n):
     buenajugada=0
     modulo=n%7
     if(modulo>=2 and modulo<=3):
@@ -23,12 +23,11 @@ def jugadabuena(n):
         buenajugada=5
     return buenajugada
 
-
-def ejecutarjuego(n):
+def Ejecutarjuego(n):
     intentos=0
     ganador=""
-    while(ganador!="El jugador 1 es el ganador del juego de las piedras" and ganador!="El jugador 2 es el ganador del juego de las piedras"):
-        jugada=jugadabuena(n)
+    while(ganador!="P1 is the winner" and ganador!="P2 is the winner"):
+        jugada=jugadaperfecta(n)
         if(jugada==0):
             if(n>5):
                 jugada=5
@@ -45,14 +44,15 @@ def ejecutarjuego(n):
 
 
 if __name__ == '__main__':
-    fptr = (sys.stdout)  # Arreglado el error que no dejaba funcionar el código, de stackoverflow
+    fptr = sys.stdout
     print("Cuántas partidas quieres jugar?")
     t = int(input().strip())
-    
+
     for t_itr in range(t):
         print("¿Cuántas piedras tienes?")
         n = int(input().strip())
         result = JuegoPiedras(n)
-        ejecutarjuego(n)
+        Ejecutarjuego(n)
         fptr.write(result + '\n')
+
     fptr.close()
