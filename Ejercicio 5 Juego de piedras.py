d0bdff581c8f0dcd6f637d6bcedb9c7a06798ff8
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2021-12-17 19:28:29 +0100

    puestos los print para indicar los datos a inputear, pero siempre da ganador al jugador 1, hay un error

--- Ejercicio 5 Juego de piedras.py
+++ Ejercicio 5 Juego de piedras.py
@@ -7,7 +7,7 @@ import sys
 def JuegoPiedras(n):
     ganador=""
     if(jugada(n)!=0):
-        ganador="El jugador 1 es el ganador de el juego de las piedras"
+        ganador="El jugador 1 es el ganador del juego de las piedras"
     else:
         ganador ="El jugador 2 es el ganador del juego de las piedras"
     return ganador
@@ -26,9 +26,11 @@ def jugada(n):
 
 if __name__ == '__main__':
     fptr = (sys.stdout)  # Arreglado el error que no dejaba funcionar el código, de stackoverflow
+    print("Cuántas partidas quieres jugar?")
     t = int(input().strip())
     for t_itr in range(t):
-    n = int(input().strip())
-    result = JuegoPiedras(n)
-    fptr.write(result + '\n')
+        print("¿Cuántas piedras tienes?")
+        n = int(input().strip())
+        result = JuegoPiedras(n)
+        fptr.write(result + '\n')
     fptr.close()
