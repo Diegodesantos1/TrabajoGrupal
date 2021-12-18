f613cddc14b8d36d65b129ddd9a329498140f983
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2021-12-18 22:38:23 +0100

    Arreglado el código, ya no da error

--- Ejercicio 7 Calificación de estudiantes.py
+++ Ejercicio 7 Calificación de estudiantes.py
@@ -5,15 +5,11 @@ import re
 import sys
 
 
-
 def gradingStudents(grades):
-    list = []
+    list=[]
     for grade in (grades):
         list.append(notas_finales(grade)) #El punto append sirve para añadir 
     return list
-        
-
-
 
 #Según esta tarea si la calificación obtenida es menor que 40 no se redondeará,
 #puesto que menor de 40 ya es supenso y aunque se redondee será suspenso.
@@ -21,39 +17,33 @@ def gradingStudents(grades):
 #Por otro lado, si hay una diferencia menor que 3 entre el siguiente número de la nota  obtenida y la nota obtenida se resondeará
 #hasta el siguiente múltiplo de 5
 
-def notas_finales(grades): 
-    nota_redondeada = 0
-    if grades < 40:
-        nota_redondeada = grades
+def notas_finales(grades):
+    nota_redondeada=0
+    if(grades<40):
+        nota_redondeada=grades
     else:
-        conciente = int(grades/5+1)
-        multiplo=conciente*5
-        if (multiplo-grades<3):
+        cociente=int(grades/5 + 1)
+        multiplo=cociente*5
+        if(multiplo-grades<3):
+            nota_redondeada=multiplo
+        else:
             nota_redondeada=grades
-    return grades
-    
-    
-
-
+    return nota_redondeada
 
 if __name__ == '__main__':
     fptr = sys.stdout
+    print("Número de estudiantes")
+    grades_count = int(input().strip())
 
-    grades_count = int(input().strip()) 
     grades = []
 
-
-  
     for _ in range(grades_count):
-        print("Nota de cada estudiante:")
+        print("Nota de cada estudiante")
         grades_item = int(input().strip())
         grades.append(grades_item)
-       
 
     result = gradingStudents(grades)
-
-    print('\n Notas redondeadas (según apliqué: \n')
-    fptr.write('\n'.join(map(str, result)))
+    fptr.write(f"Las notas finales son:{result}")
     fptr.write('\n')
 
     fptr.close()
