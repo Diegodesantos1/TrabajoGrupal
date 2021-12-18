98f1c33f14208b16b765d006305fd15009bccee7
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2021-12-18 20:51:30 +0100

    Puestos los prints, falta arreglar alguno

--- Ejercicio 8 La manzana y la naranja.py
+++ Ejercicio 8 La manzana y la naranja.py
@@ -19,14 +19,19 @@ def contarmanzanasnaranjas(s, t, a, b, apples, oranges):
 
 
 if __name__ == '__main__':
+    print("Establece el punto de inicio y final de la ubicación de la casa de Sam separado por un espacio")
     first_multiple_input = input().rstrip().split()
     s = int(first_multiple_input[0])
     t = int(first_multiple_input[1])
+    print("Establece la ubicación del manzano y naranjo separados por un espacio")
     second_multiple_input = input().rstrip().split()
     a = int(second_multiple_input[0])
     b = int(second_multiple_input[1])
+    print("Establece las distancias a las que cada manzana cae del árbol")
     third_multiple_input = input().rstrip().split()
+    print("Establece las distancias a las que cada naranja cae del árbol")
     m = int(third_multiple_input[0])
+    print("Establece las distancias a las que cada naranja cae del árbol")
     n = int(third_multiple_input[1])
     apples = list(map(int, input().rstrip().split()))
     oranges = list(map(int, input().rstrip().split()))
