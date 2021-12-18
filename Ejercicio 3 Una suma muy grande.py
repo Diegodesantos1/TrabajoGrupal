88165b9ddd3ca052434995bfcb7d54579e365953
albabernal03 <ALBABERO@MYUAX.COM>
2021-12-18 21:36:51 +0100

    escribo dos print que muestre lo seleccionado

--- Ejercicio 3 Una suma muy grande.py
+++ Ejercicio 3 Una suma muy grande.py
@@ -15,10 +15,16 @@ def aVeryBigSum(ar):
 
 if __name__ == '__main__':
     fptr = sys.stdout #código perteneciente a satckoverflow
+
     print('Escribe un número:')
     ar_count = int(input().strip())
+    selección = print(ar_count)
+
     print('Escriba una sucesión de números separados por espacios:')
     ar = list(map(int, input().rstrip().split()))
+    seleccion_2 = print(ar)
+
     result = aVeryBigSum(ar)
     fptr.write(str(result) + '\n')
+    print('La solución es igual:', result)
     fptr.close()
