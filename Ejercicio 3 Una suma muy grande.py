b71159d3080d75e9e8829d3b04bbd6c0c1ced72c
albabernal03 <ALBABERO@MYUAX.COM>
2021-12-18 21:25:43 +0100

    elimino lo que estaba en comentarios que es innecesario

--- Ejercicio 3 Una suma muy grande.py
+++ Ejercicio 3 Una suma muy grande.py
@@ -1,19 +1,16 @@
-#!/bin/python3
+
 import math
 import os
 import random
 import re
 import sys
-#
-# Complete the 'aVeryBigSum' function below.
-#
-# The function is expected to return a LONG_INTEGER.
-# The function accepts LONG_INTEGER_ARRAY ar as parameter.
-#
+
 def aVeryBigSum(ar):
     suma = 0
     for i in ar:
-        
+        suma += i
+    return suma
+
 
 
 if __name__ == '__main__':
