351d61262333e8cd472884472bee83e53a8daf4e
albabernal03 <ALBABERO@MYUAX.COM>
2021-12-18 20:18:56 +0100

    tabulamos el código

--- Ejercicio 3 Comparar los problemas.py
+++ Ejercicio 3 Comparar los problemas.py
@@ -15,10 +15,10 @@ import sys
 def compareTriplets(a, b):
 # Write your code here
 if __name__ == '__main__':
-fptr = open(os.environ['OUTPUT_PATH'], 'w')
-a = list(map(int, input().rstrip().split()))
-b = list(map(int, input().rstrip().split()))
-result = compareTriplets(a, b)
-fptr.write(' '.join(map(str, result)))
-fptr.write('\n')
-fptr.close()
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    a = list(map(int, input().rstrip().split()))
+    b = list(map(int, input().rstrip().split()))
+    result = compareTriplets(a, b)
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+    fptr.close()
